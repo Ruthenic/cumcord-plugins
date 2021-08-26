cf1ae6ca0d30cfe08964262cb41e047f5bc8c424
Ruthenic <Ruthenic@users.noreply.github.com>
2021-08-26 01:49:42 +0000

    deploy: 12185683f09860f92432aa7415dbfe57e037c981

--- build_plugin_index.py
+++ build_plugin_index.py
@@ -2,8 +2,15 @@ import json, sys
 # Licensed under WTFPL, please steal for your own plugin websites
 
 def build_html(name, author, description, license, folder):
-    return "<head><style>@import url('https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@300&display=swap');*{font-family: 'Azeret Mono', monospace;}</style></head>" + f"<body><a href='..'>Back to Home</a><h1>{name}</h1><h4>by {author}<hr></h4><p>{description}</p><a href='https://cumcord.ruthenic.com/{folder}' style='position: absolute; bottom: 5px;'>Add to Cumcord</a></body>"
-
+    return """
+    <head>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@300&display=swap');
+            *{font-family: 'Azeret Mono', monospace;}
+        </style>
+        <script>var timesClicked = 0;</script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    </head>""" + f"""<body><a href='..'>Back to Home</a><h1>{name}</h1><h4>by {author}<hr></h4><p>{description}</p><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script><script>function copyLink()""" + """{let loc = window.location.href.replace("index.html", "");navigator.clipboard.writeText(loc);Toastify({text: "Copied link to clipboard!", "background-color": "#404040", gravity: "bottom", position: "right"}).showToast();timesClicked++;}</script><button style='position: absolute; bottom: 5px;' onclick='copyLink()'>Add to Cumcord</button></body>"""
 imp, out, folder = sys.argv[1], sys.argv[2], sys.argv[3]
 imp = json.load(open(imp))
 print(imp)
