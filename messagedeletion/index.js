015263230008729a42eb20ca128199fe4f5b9d5a
Ruthenic <mdrakea3@tutanota.com>
2021-08-27 14:50:12 -0400

    cleaner code

--- messagedeletion/index.js
+++ messagedeletion/index.js
@@ -1,19 +1,19 @@
 const getElementFromMessageId = cumcord.modules.webpackModules.findByProps("getElementFromMessageId").getElementFromMessageId;
+const origDispatch = Object.getPrototypeOf(cumcord.modules.webpackModules.findByProps("dispatch"));
 
-window.deletedMessages = []
+let deletedMessages = []
 
 let unpatch;
 let untimeout;
+let uncss
 
 function styleMessages() {
-	for (let message of window.deletedMessages) {
+	for (let message of deletedMessages) {
 		try {
-			/*message["deletedHtmlElement"].style.setProperty("color", "#f04747", "important");
-			document.getElementById(message["id"]).style.setProperty("color", "#f04747", "important");*/ //MLV2 style
-																									   //TODO: make this shit work
-			message["deletedHtmlElement"].style.backgroundColor = 'rgba(240, 71, 71, 0.1)';
-			document.getElementById(message["id"]).style.backgroundColor = 'rgba(240, 71, 71, 0.1)'; //Goosemod style
-																									   //TODO: when cumcord adds settings, allow a toggle between the two
+			if (!document.getElementById(message["id"]).classList.contains('deleted-message')) {
+				document.getElementById(message["id"]).classList.add('deleted-message');
+				//TODO: do inline styles anyways? they're way more consistent
+			}
 		} catch (error) {
 			//console.log(error)
 		}
@@ -22,29 +22,39 @@ function styleMessages() {
 
 export default {
     onLoad() {
+    	//MLv2 style
+    	//TODO: make this shit work
+    	//uncss = cumcord.patcher.injectCSS(".deleted-message{color: #f04747 !important;}");
+    	
+   		//GM Style
+    	uncss = cumcord.patcher.injectCSS(".deleted-message{background-color: rgba(240, 71, 71, 0.1);}");
+		//TODO: when cumcord adds settings, allow a toggle between the two (assuming i ever figure out how to change text colour)
+    	
     	untimeout = setInterval(styleMessages, 300);
-        unpatch = cumcord.patcher.instead("dispatch", Object.getPrototypeOf(cumcord.modules.webpackModules.findByProps("dispatch")), (args, orig) => { //"prototype bullshit", thanks creatable
+        unpatch = cumcord.patcher.instead("dispatch", origDispatch, (args, orig) => { //"prototype bullshit", thanks creatable
             if (args[0]["type"] === "MESSAGE_DELETE") {
+            	//console.log(args);
                 try {
                     var deletedMessageInfo = {"deletedHtmlElement": getElementFromMessageId(args[0]["id"])};
                     deletedMessageInfo["deletedText"] = deletedMessageInfo["deletedHtmlElement"].innerText.split("\n")[3];
-                    deletedMessageInfo["deletedHtmlElement"].style.backgroundColor = 'rgba(240, 71, 71, 0.1)';
                     deletedMessageInfo["id"] = "chat-messages-" + args[0]["id"];
+                    deletedMessageInfo["deletedHtmlElement"];
                     console.log("[messagedeletion] New Deleted Message: " + deletedMessageInfo["deletedText"]);
-					if (window.deletedMessages.indexOf(deletedMessageInfo) == -1) {
-                    	window.deletedMessages.push(deletedMessageInfo);
+					if (deletedMessages.indexOf(deletedMessageInfo) == -1) {
+                    	deletedMessages.push(deletedMessageInfo);
 					}
-                    console.log(window.deletedMessages);
+                    //console.log(deletedMessages);
                 	return;
                 } catch (error) {
                     console.log("[messagedeletion] Failed to get deleted message!");
-                    console.log(error);
+                    console.error(error);
                 }
             }
             return orig(...args);
         });
     },
     onUnload() {
+    	uncss();
         unpatch();
         clearInterval(untimeout);
     }
