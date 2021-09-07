b69a7f54bf0efb0e5b7a777e5ecd9a504c1cb5ad
Ruthenic <Ruthenic@users.noreply.github.com>
2021-09-07 05:12:25 +0000

    deploy: 32227f0c044a1eeb9baa642014e1d28b0ed51a62

---
+++ messagedeletion/components/SettingCard.jsx
@@ -0,0 +1,20 @@
+import { React } from "@cumcord/modules/common";
+const webpackModules = cumcord.modules.webpackModules;
+
+const useState = React.useState;
+const Switch = webpackModules.findByDisplayName("Switch");
+const Text = webpackModules.findByDisplayName("Text");
+const Header = webpackModules.findByDisplayName("Header");
+const SwitchItem = webpackModules.findByDisplayName("SwitchItem");
+
+export default (props) => {
+    const [enabled, setEnabled] = useState(props.enabled);
+    return (
+        <SwitchItem value={props.value} note={props.note} onChange={() => {
+            setEnabled(!enabled);
+            props.onChange(); //TODO: sync between the props and window object
+        }} value={enabled}>
+            {props.name}
+        </SwitchItem>
+    )
+}
