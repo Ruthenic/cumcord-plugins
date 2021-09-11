3afc1d6666d8b7f70de42fb7a2b8e5b6aecb2602
Ruthenic <mdrakea3@tutanota.com>
2021-09-10 20:07:48 -0400

    fine you win

--- build_plugin_index.py
+++ build_plugin_index.py
@@ -11,7 +11,7 @@ def build_html(name, author, description, license, folder):
         <script>var timesClicked = 0;</script>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
         <script src="/webscripts/install.js"></script>
-    </head>""" + f"""<body><a href='..'>Back to Home</a><h1>{name}</h1><h4>by {author}<hr></h4><p>{description}</p><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script><button style='position: absolute; bottom: 5px;' onclick='copyLink()'>Add to Cumcord</button></body>"""
+    </head>""" + f"""<body><a href='..'>Back to Home</a><h1>{name}</h1><h4>by {author}<hr></h4><p>{description}</p><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script><button onclick='copyLink()'>Add to Cumcord</button></body>"""
 imp, out, folder = sys.argv[1], sys.argv[2], sys.argv[3]
 imp = json.load(open(imp))
 print(imp)
