2277f45208fc7e315bdb7a597405da3a5de69fd2
Ruthenic <mdrakea3@tutanota.com>
2022-06-11 00:24:58 -0400

    ez fix

--- noReplyMention/index.js
+++ noReplyMention/index.js
@@ -2,7 +2,7 @@ let unpatch;
 
 export default {
     onLoad() {
-        unpatch = cumcord.patcher.before("createPendingReply", cumcord.modules.webpackModules.findByProps("createPendingReply"), (args) => {
+        unpatch = cumcord.patcher.before("createPendingReply", cumcord.modules.webpack.findByProps("createPendingReply"), (args) => {
         	args[0].shouldMention = false;
         });
     },
